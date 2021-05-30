6c390d8c27494b19a78285e8bf8fc1f173638d11
IamHalid <vmhunterjob@gmail.com>
2021-05-31 00:59:14 +0800

    modified src-app-js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <div className="content">
+     <div className="content"> 
        <h1>App Component</h1>
      </div>
     </div>
