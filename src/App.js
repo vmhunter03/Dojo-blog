906e87912f47f732dbbc13b608bb218b79ff4882
IamHalid <vmhunterjob@gmail.com>
2021-05-31 01:25:06 +0800

    Multiple Components_Added a new files App.js & Home.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const link = "http://www.google.com";
   return (
     <div className="App">
+      <Navbar />
      <div className="content"> 
-       <h1>{ title }</h1>
-       <a href={link}>Google Site</a>
+       <Home />
+       
      </div>
     </div>
   );
